3e4ac557a55cdce7ef13e22e0446a7867b990c89
Chanachai Puttaruksa <chanachai_mig@hotmail.com>
2023-05-29 17:54:02 +0700

    Fix: format some code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,29 @@
 import styles from '../shared/globals.module.css'
 import Header from '../components/header'
 import Navigation from '../components/navigation'
+import { InferGetStaticPropsType } from 'next'
+import { getPostList } from '../shared/util' 
 
-export default function Home() {
+type PostList = string[]
+
+export default function Home({ posts }: InferGetStaticPropsType<typeof getStaticProps>) {
+  console.log("posts", posts)
   return (
     <>
       <Navigation />
       <main className={styles.main}>
         <Header />
         <h1>Next Website</h1>
+        <hr />
+        {posts && posts.length > 0 && (
+          <ul>
+            {posts.map((slug) => (
+              <li key={slug}>
+                {slug.replace(/-/g, ' ')}
+              </li>
+            ))}
+          </ul>
+        )}
         <hr />
         <h4>Adapt from udemy course</h4>
         <a href="https://www.udemy.com/course/nextjs-up-and-running/">
@@ -17,4 +32,15 @@ export default function Home() {
       </main>
     </>
   )
+}
+
+export const getStaticProps = async () => {
+  console.log("get static props")
+  const posts: PostList = getPostList()
+  console.log("get static props", posts)
+  return {
+    props: {
+      posts
+    }
+  }
 }
---
+++ pages/posts/hello-there.tsx
@@ -0,0 +1,9 @@
+const Hello = () => {
+    return (
+        <div>
+            <h1>Hello There !</h1>
+        </div>
+    )
+}
+
+export default Hello
